602794258c9573ee9303b64c5004028809065c6d
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2026-05-17 00:14:16 +0300

    add:types

--- 01_intro/01_intro/01_intro.cpp
+++ 01_intro/01_intro/01_intro.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+    cout << "Hello World!\n";
 
+    cout << "My name is " << "Vasia" << "\n";
+
+    // Hello World!\nMy name is Vasia\n
+
+    //      \t \n \b \r
+
+    cout << "value\\new" << "\n";
+
+    cout << "My name is \"Vasia\"" << "\n";
+
+    cout << "id\temail\tpassword";
+    
+    return 0;
+
+    //
+
+    // Ctrl + K + C
+
+    /*
+        first
+        second
+    */
+
+}
---
+++ 01_intro/02_types/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+int main()
+{
+	int a;			// 4b
+
+	unsigned int b;	// 4b
+
+	char c;			// 1b
+
+	return 0;
+}
+
+
+
+
+
