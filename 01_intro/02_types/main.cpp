c65f920a54a7904ecb07958e5c30cf3c1c5c7318
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2026-05-23 23:46:58 +0300

    add: types

--- 01_intro/02_types/main.cpp
+++ 01_intro/02_types/main.cpp
@@ -1,16 +1,156 @@
 #include <iostream>
 
+#pragma region Variables
+
+//int main()
+//{
+//	//int value;
+//
+//	//int Value;
+//
+//	//int value;		// Неинициализированная
+//	//value = 12;		// Присваивание
+//
+//	// ИНИЦИАЛИЗАЦИЯ
+//
+//	//int a = 12;		// Инициализация присваиванием  :-)
+//
+//	//int b(12);		// Классическая инициализация	:-|
+//
+//	//int c{ 12 };	// Унифицированная инициализация :-)))
+//
+//
+//	//int d = 140;
+//	//std::cout << d;
+//
+//	//int value{ 23 };
+//	//std::cout << value;
+//
+//
+//	//int a{};
+//	//// >>> EQUALS <<<
+//	//int b{ 0 };
+//
+//	//std::cout << a;
+//
+//
+//	//int age{ 18 };
+//	//std::cout << age << '\n';
+//
+//	//age = 23;
+//	//std::cout << age << '\n';
+//
+//	return 0;
+//}
+
+#pragma endregion
+
+
+#pragma region Types
+
+// ---- Логический тип (булевый) ----
+
+//int main()
+//{
+//	bool a{ true };
+//	bool b{ false };
+//
+//	std::cout << a << '\n';
+//	std::cout << b << '\n';
+//
+//	return 0;
+//}
+
+
+// ---- Целочисленные типы ----
 
 int main()
 {
-	int a;			// 4b
+	//signed char a{ 'x' };		// 1 byte		[-127; 128]
+	//// >>> EQUALS <<<
+	//char b{ 'x' };
+	//unsigned char c{ 'x' };		// 1 byte		[0; 255]
+	//char d{ 99 };
 
-	unsigned int b;	// 4b
+	//short e{ 100 };				// 2 byte		[-32768; 32767]
 
-	char c;			// 1b
+	//unsigned short f{ 100 };	// 2 byte		[0; 65535]
 
-	return 0;
+	//int g{ 100 };				// 4 byte		[-2147483647; 2147483646]
+
+	//unsigned int h{ 100 };		// 4 byte		[0; 4294967295]
+
+	////--- переполнение типа
+	//unsigned int xxx{ 4294967295 };
+	//std::cout << xxx << '\n';
+	//xxx = xxx + 1;
+	//std::cout << xxx << '\n';
+	////---
+
+	//long i{ 100 };						// 8 byte
+
+	//unsigned long j{ 100 };				// 8 byte
+
+	//long long k{ 100 };					// 16 byte
+
+	//unsigned long long k{ 100 };		// 16 byte
+
+
+
+
+	//unsigned int a{ 1000U };
+	//std::cout << a << '\n';
+
+	// U - unsigned int
+	// L - long
+	// UL - unsigned long
+	// LL - long long
+	// ULL - unsigned long long
+
+
+	//int a{ 0x1A };				// 26
+	//std::cout << a << '\n';
+
+	//int b{ 0xFF };				// 255
+
+	//int c{ 0xFFFF };
+
+	//int d{ 035 };				// 29
+	//std::cout << d << '\n';
+
+	//int e{ 0b00000101 };
+	//std::cout << e;
+
+
+
+	
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#pragma endregion
+
+
+
+
+
 
 
 
